ae727e3219badf1d6a0f3c96a02d8612e5a6000c
Gwannon <monclus.jorge@gmail.com>
2023-04-20 12:18:01 +0200

    add/delete/update emails

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Librería de PHP para trabajar contra la API de Clientify
 
 ## Instalación
 Renombra config.dist.php como config.php y configura tu API Key de Clientify.
+
+## Notas
+Los tags, emails, teléfonos y direcciones no se actualizan en la API hasta que no se ejecuta las funcione updateTags(), updateEmails(), ... Es una forma de ahorrar llamadas a la API concentrandolas en una sola función.
+
+## Todo
+* add/delete/update direcciones
+* add/delete/update teléfonos
+
+## Tipos de emails, direcciones o télefonos
+| id | tipo    |
+|----|---------|
+| 1  | trabajo |
+| 2  | personal|
+| 3  | otros   |
+| 4  | principal|
--- src/classes/contact.php
+++ src/classes/contact.php
@@ -4,9 +4,9 @@ class contactClientify {
   public $id;
   public $firstName;
   public $lastName;
-  public $emails;
+  private $emails;
   public $phones;
-  public $tags;
+  private $tags;
   public $status;
 
   /*
@@ -17,8 +17,6 @@ class contactClientify {
     3	Other
     4	Main
   */
-
-
 
   public function __construct($id = 0, $createIfNotExists = false) {
 
@@ -85,6 +83,58 @@ class contactClientify {
     $response = curlClientfyCallPut("/contacts/{$this->id}/", json_encode($payload));
   }
 
+  /* EMAILS */
+  public function addEmail($email, $type) { 
+    $email = strtolower($email);
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) && 
+      !array_search($email, array_column(json_decode(json_encode($this->emails),TRUE), 'email'))) {
+      $this->emails[] = (object) [
+        "id" => 0,
+        "type" => $type,
+        "email" => $email
+      ];
+      return true;
+    }
+    return false;
+  }
+
+  public function deleteEmail($email) { 
+    $key = array_search(strtolower($email), array_column(json_decode(json_encode($this->emails),TRUE), 'email'));
+    if(isset($this->emails[$key])) {
+      unset($this->emails[$key]);
+      return true;
+    }
+    return false;
+  }
+
+  public function getEmails() { return $this->emails; }
+
+  public function updateEmails() {    
+    $deleteEmails = [];
+    foreach($this->emails as $email) {
+      if($email->id == 0) $response = curlClientfyCallPost("/contacts/{$this->id}/emails/", json_encode(["email" => $email->email, "type" => $email->type]));
+    }
+
+    $response = curlClientfyCall("/contacts/{$this->id}/emails/");
+    $currentEmails = $response->results;
+    foreach($currentEmails as $currentEmail) {
+      $controlDelete = 0;
+      foreach($this->emails as $email) {
+        if($email->email == $currentEmail->email) {
+          $controlDelete = 1;
+        }
+      }
+      if($controlDelete == 0) $response = curlClientfyCallDelete("/contacts/{$this->id}/emails/{$currentEmail->id}/");
+    }
+  }
+
+  /* ADDRESSES */
+
+  /* PHONES */
+
+  /* TAGS */
+  public function getTags() { return $this->tags; }
+
   public function addTag($tag) { $this->tags[] = $tag; return true; }
 
   public function deleteTag($tag) { $this->tags = array_diff($this->tags, [$tag]); }
@@ -115,7 +165,7 @@ class contactClientify {
     }
   }
 
-
+  /* OTHERS */
   public static function existsContact($id) {
     if (is_numeric($id) && $id > 0) {
       $response = curlClientfyCall("/contacts/{$id}/");
@@ -123,8 +173,11 @@ class contactClientify {
       $temp = curlClientfyCall("/contacts/?query={$id}&email={$id}");
       $response = (count($temp->results) > 0 ? $temp->results[0] : false);
     }
-    if(isset($response) && is_object($response) && isset($response->id) && is_numeric($response->id) && $response->id > 0) return true;
+    if(isset($response) && 
+      is_object($response) && 
+      isset($response->id) && 
+      is_numeric($response->id) && 
+      $response->id > 0) return true;
     return false;
   }
-
 }
